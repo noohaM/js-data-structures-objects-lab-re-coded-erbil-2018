dbed0fff03917cc5966b4f02441ee58bf0afb48c
nooha mohammed <noohamohammed00@gmail.com>
2018-09-05 18:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const driver= {name:'sam'}
 
 function updateDriverWithKeyAndValue(driver, key, value)
 {
-  let driver = object.assign({},driver,)
+  let driver = object.assign({},driver,{[key]:value})
   
   return
 }
